632e4ace7e705ed1485a6d70d8a910c24c9e47a7
FrancoisJohannson <erhard.janak@t-online.de>
2019-04-28 09:46:47 +0200

    Processing a first POST

--- src/main/java/francois/johannson/HumbleController.java
+++ src/main/java/francois/johannson/HumbleController.java
@@ -1,13 +1,16 @@
 package francois.johannson;
 
 import org.json.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class HumbleController {
+
+    @PostMapping(path = "/members")
+    public void addMember(@RequestBody String member) {
+        System.out.println("Processing a POST");
+    }
 
     @GetMapping("/infos")
     public String infos() {
